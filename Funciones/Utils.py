2d4f838bcf02e6178c203d807c3b864d0ff18c24
Leam <llquinteros@gmail.com>
2025-06-14 17:49:03 -0300

    Se completo la carga de naves.
    - Se logro insertar cada tipo de nave, validando que no se superpongan naves y que no haya naves que queden a la mitad fuera del mapa.

--- Funciones/Utils.py
+++ Funciones/Utils.py
@@ -19,21 +19,84 @@ def inicializar_matriz(cantidad_filas: int, cantidad_columnas:int, valor_inicial
         matriz += [fila]
     return matriz
 
-def cargar_naves(tablero:list) -> list:
-    for i in range(40):        
-        
-        rnd_fila = random.randint(0, len(tablero) - 1)
-        rnd_col =  random.randint(0, len(tablero) - 1)  
-    
-        while tablero[rnd_fila][rnd_col] == 1: 
-            print(f"Se superpusieron submarinos. Fila: {rnd_fila}, Col: {rnd_col}")
-            rnd_fila = random.randint(0, len(tablero) - 1)
-            rnd_col =  random.randint(0, len(tablero) - 1)        
-            
 
-        tablero[rnd_fila][rnd_col] = 1
+
+def cargar_naves(tablero:list) -> list:
+    contador = 0
+
+    while contador < 10:
+        rnd_fila = random.randint(0, len(tablero) - 1)
+        rnd_col =  random.randint(0, len(tablero) - 1)
+        
+        # Submarinos
+        if contador < 4:  
+            if tablero[rnd_fila][rnd_col] == 1:
+                continue          
+            
+            tablero[rnd_fila][rnd_col] = 1
+            print(f"Submarino: [{[rnd_fila]}] [{[rnd_col]}]")
+        
+        # Destructores
+        elif contador > 3 and contador < 7:
+            if rnd_col > 8:
+                continue
+
+            if tablero[rnd_fila][rnd_col] == 1 or tablero[rnd_fila][rnd_col + 1] == 1:
+                continue 
+            
+            tablero[rnd_fila][rnd_col] = 1
+            tablero[rnd_fila][rnd_col + 1] = 1
+            print(f"Destructor: [{[rnd_fila]}] [{[rnd_col]}]")
+            print(f"Destructor: [{[rnd_fila]}] [{[rnd_col + 1]}]")
+        
+        # Cruceros
+        elif contador > 6 and contador < 9:
+            if rnd_col > 7:
+                continue
+            
+            if tablero[rnd_fila][rnd_col] == 1 or tablero[rnd_fila][rnd_col + 1] == 1 or tablero[rnd_fila][rnd_col + 2] == 1:
+                continue
+
+            tablero[rnd_fila][rnd_col] = 1
+            tablero[rnd_fila][rnd_col + 1] = 1
+            tablero[rnd_fila][rnd_col + 2] = 1
+            print(f"Crucero: [{[rnd_fila]}] [{[rnd_col]}]")
+            print(f"Crucero: [{[rnd_fila]}] [{[rnd_col + 1]}]")
+            print(f"Crucero: [{[rnd_fila]}] [{[rnd_col + 2]}]")
+        
+        # Acorazados
+        elif contador > 8:
+            if rnd_col > 6:
+                continue
+            
+            if tablero[rnd_fila][rnd_col] == 1 or tablero[rnd_fila][rnd_col + 1] == 1 or tablero[rnd_fila][rnd_col + 2] == 1 or tablero[rnd_fila][rnd_col + 3] == 1:
+                continue
+
+            tablero[rnd_fila][rnd_col] = 1
+            tablero[rnd_fila][rnd_col + 1] = 1
+            tablero[rnd_fila][rnd_col + 2] = 1
+            tablero[rnd_fila][rnd_col + 3] = 1  
+            print(f"Acorazado: [{[rnd_fila]}] [{[rnd_col]}]")     
+            print(f"Acorazado: [{[rnd_fila]}] [{[rnd_col + 1]}]")       
+            print(f"Acorazado: [{[rnd_fila]}] [{[rnd_col + 2]}]")       
+            print(f"Acorazado: [{[rnd_fila]}] [{[rnd_col + 3]}]")               
+        
+        contador += 1
 
     return tablero
+
+
+# Validaciones:
+# Validar que no se superpongan las naves.
+# Validar que no queren la mitad de una nave fuera de la matriz.
+
+
+# detectar donde si ya hay una nave en esa posicion.
+# generar naves
+#    - Submarinos   : [fila] [col] = 1
+#    - Destructores : [fila] [col] [col] = 1
+#    - Crucero      : [fila] [col] [col] [col] = 1
+#    - Acorazado    : [fila] [col] [col] [col] [col] = 1
 
 
 
